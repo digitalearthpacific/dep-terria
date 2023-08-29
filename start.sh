ed9ba8d2a76b9a46e4925d7576a3641c3f423b14
Sachindra Singh <sachindras@spc.int>
2023-08-29 13:31:41 +1200

    Product Init Registration.

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export NODE_OPTIONS=--max_old_space_size=4096
 export NODE_OPTIONS=--openssl-legacy-provider
 yarn install && yarn gulp && yarn start
+#yarn gulp release
+#node ./node_modules/terriajs-server/terriajs-server.js
 
